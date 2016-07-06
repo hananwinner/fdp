c44f076d2f865b272ebf20a366900be7ad9c8efd
BenjiBackslash <hanan888@gmail.com>
2016-07-06 13:52:08 +0300

    make_em_fragments_test_simple_wizard: cosmetics.

--- app/src/main/java/com/fractureof/demos/location/TestDateTimeWizardModel.java
+++ app/src/main/java/com/fractureof/demos/location/TestDateTimeWizardModel.java
@@ -10,13 +10,18 @@ import com.fractureof.demos.location.wizard.model.DateTimePage;
  * Created by tyler on 19/06/2016.
  */
 public class TestDateTimeWizardModel extends AbstractWizardModel {
+
+    static class Consts {
+        public static String DATESTIME_PAGE_TITLE = "Date's Time";
+    }
+
     public TestDateTimeWizardModel(Context context) {
         super(context);
     }
     @Override
     protected PageList onNewRootPageList() {
         return new PageList(
-                new DateTimePage(this,"Date's Time").setRequired(true)
+                new DateTimePage(this,Consts.DATESTIME_PAGE_TITLE).setRequired(true)
         );
     }
 }
